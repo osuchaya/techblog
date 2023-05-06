f4457a5112d89ec71fe720ffe961937c68c82d46
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-05 18:01:33 -0700

    Basic folder structures created and tested basic routes

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+//const apiRoutes = require('./api');
+//const 
+router.get('/', async (req,res) => {
+    res.render('homepage', {})
+})
+router.get('/login', async (req,res) => {
+    
+})
+module.exports = router;
