a594308471faccd47a701311bfc5abbeb2c64501
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-06 16:27:52 -0700

    added login code for controller

--- controllers/index.js
+++ controllers/index.js
@@ -19,14 +19,41 @@ router.get("/", async (req, res) => {
   res.render("homepage", { blogposts });
 });
 router.get("/login", async (req, res) => {
-    res.render("login")
+  res.render("login");
 });
 
 router.get("/dashboard", async (req, res) => {
-    res.render("dashboard")
+  res.render("dashboard");
 });
 
+router.post("/api/users/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
 
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json({err, message: "Error!"});
+  }
+});
 
 module.exports = router;
 
