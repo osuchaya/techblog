d2d1ee494947b0e503f8878d8a872812a80721ce
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-05 19:08:29 -0700

    Drafted User Model

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,9 @@ router.get('/', async (req,res) => {
     res.render('homepage', {})
 })
 router.get('/login', async (req,res) => {
-    
+
 })
 module.exports = router;
+
+//user can write (POST/DELETE)has many blogpost
+//blogpost can be viewed by many users (GET only)
