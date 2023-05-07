5de7f82cee733f8129b15310ea58480798676deb
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-07 13:57:36 -0700

    Added routes files in controller folder

--- controllers/index.js
+++ controllers/index.js
@@ -26,36 +26,53 @@ router.get("/dashboard", async (req, res) => {
   res.render("dashboard");
 });
 
-router.post("/api/users/login", async (req, res) => {
-    console.log(req.body)
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
-    const validPassword = await userData.checkPassword(req.body.password);
+// router.post("/api/users/login", async (req, res) => {
+//     console.log(req.body)
+//   try {
+//     const userData = await User.findOne({ where: { email: req.body.email } });
+//     if (!userData) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect email or password, please try again" });
+//       return;
+//     }
+//     const validPassword = await userData.checkPassword(req.body.password);
 
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
+//     if (!validPassword) {
+//       res
+//         .status(400)
+//         .json({ message: "Incorrect email or password, please try again" });
+//       return;
+//     }
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+//     req.session.save(() => {
+//       req.session.user_id = userData.id;
+//       req.session.logged_in = true;
 
-      res.json({ user: userData, message: "You are now logged in!" });
-    });
-  } catch (err) {
-    res.status(400).json({err, message: "Error!"});
-  }
-});
+//       res.json({ user: userData, message: "You are now logged in!" });
+//     });
+//   } catch (err) {
+//     res.status(400).json({err, message: "Error!"});
+//   }
+// });
 
+
+// router.post('/logout', (req, res) => {
+
+//     console.log(req.session.logged_in)
+//     if (req.session.logged_in) {
+  
+//       req.session.destroy(() => {
+       
+//         res.status(204).end();
+//         res.redirect('/login');
+//       });
+//     } else {
+//       res.status(404).end();
+//       res.render('login');
+//     }
+//   });
+  
 module.exports = router;
 
 //user can write (POST/DELETE)has many blogpost
