cda518aa69c4e3f2d6a141347f7c332adfca94a9
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-05-08 21:02:46 -0700

    Add comment functionality and attempt to get comment creator

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./users');
 const Blog = require('./blog');
+const Comment = require('./comment');
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
@@ -10,4 +11,20 @@ Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
+Comment.hasOne(User, {
+    foreignKey: 'user_id',
+});
+
+// User.belongsTo(Comment, {
+//     foreignKey: 'comment_id',
+// });
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Blog, Comment };
--- public/js/addcomment.js
+++ public/js/addcomment.js
@@ -2,11 +2,11 @@ const addCommentHandler = async (event) => {
     event.preventDefault();
 
     const comment = document.querySelector('#comment').value.trim();
-    
+    const blog_id = document.querySelector('#blog-id').value.trim();
     if (comment) {
         const response = await fetch('/addcomment', {
             method: 'POST',
-            body: JSON.stringify({ comment: comment, date_created: new Date(), user_id: 0 }), //transform JS obj into string
+            body: JSON.stringify({ content: comment, date_created: new Date(), user_id: 0, blog_id: blog_id }), //transform JS obj into string
             headers: { 'Content-Type': 'application/json' },  //tell the mailman in HTTP what is inside the letter (JSON object) e.g. fragile sticker on box
         });
 
